90b983c74cd996b99ee3eca00c03deb227acda33
Mat Lowery <mlowery@ebaysf.com>
2014-05-22 00:10:35 -0500

    Fix path

--- post-receive/trove-integration.bash
+++ post-receive/trove-integration.bash
@@ -8,7 +8,12 @@ main() {
 
 my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 my_file="${BASH_SOURCE[0]}"
-source $my_dir/vm.bash
+if [[ -f $my_dir/vm.bash ]]; then
+    source $my_dir/vm.bash
+else
+    source $my_dir/../lib/vm.bash
+fi
+
 case $1 in
     --check-vars)
     post_receive_check_vars $my_file "$2"
